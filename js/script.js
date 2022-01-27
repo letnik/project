8d4d5cd4755f2176d118edb280aaa410e028c041
Ostap Letnyanchyk <ostap.letnyanchyk@gmail.com>
2022-01-27 23:54:49 +0200

    starter

---
+++ js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+// console.log(obj);
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt("І шо скажеш?", ""); 
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Як Вас звати?', '');
+// answers[1] = prompt('Ваше прізвише', '');
+// answers[2] = +prompt('Скільки Вам років?', ''); 
+// унарний плюс - плюс який перетворює строку в число
+
+// console.log(typeof(answers));
+// console.log(answers);
+// document.write('<div style="color: red">' + answers + '</div>');
+
+// Інтерполяція
+
+// const category = 'cars';
+
+// console.log(`https://site.com/${category}/5`);
+
+// // інкримент, декримент
+// let incr = 10,
+//     decr = 10;
+
+// // оператор інкримента
+// incr++;  // постфіксна форма інкримента
+// // ++in cr;  // префіксна форма інкримента
+
+// // оператор декотмента
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// залишок ділення
+// console.log(8%3);
+
+// && - and
+// || - or
+
+// const isChecked = true,
+//       isClose = false;
+ 
+// console.log(isChecked || isClose);
+
+
